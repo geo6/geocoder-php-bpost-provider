0362d3426c02a8ead9392c2dab24204138e48913
Jonathan Beliën <jbe@geo6.be>
2018-10-26 11:23:20 +0200

    Add PHPUnit tests

---
+++ Tests/IntegrationTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\bpost\Tests;
+
+use Geocoder\IntegrationTest\ProviderIntegrationTest;
+use Geocoder\Provider\bpost\bpost;
+use Http\Client\HttpClient;
+
+class IntegrationTest extends ProviderIntegrationTest
+{
+    protected $testAddress = true;
+
+    protected $testReverse = false;
+
+    protected $testIpv4 = false;
+
+    protected $testIpv6 = false;
+
+    protected $skippedTests = [
+        'testGeocodeQuery' => 'Belgium only.',
+    ];
+
+    protected function createProvider(HttpClient $httpClient)
+    {
+        return new bpost($httpClient, 'Geocoder PHP/bpost Provider/Integration Test');
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    protected function getApiKey()
+    {
+        return null;
+    }
+}
---
+++ Tests/bpostTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\bpost\Tests;
+
+use Geocoder\Collection;
+use Geocoder\IntegrationTest\BaseTestCase;
+use Geocoder\Location;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Provider\bpost\bpost;
+
+class bpostTest extends BaseTestCase
+{
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The bpost provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithLocalhostIPv4()
+    {
+        $provider = new bpost($this->getMockedHttpClient(), 'Geocoder PHP/bpost Provider/bpost Test');
+        $provider->geocodeQuery(GeocodeQuery::create('127.0.0.1'));
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The bpost provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithLocalhostIPv6()
+    {
+        $provider = new bpost($this->getMockedHttpClient(), 'Geocoder PHP/bpost Provider/bpost Test');
+        $provider->geocodeQuery(GeocodeQuery::create('::1'));
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The bpost provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithRealIPv6()
+    {
+        $provider = new bpost($this->getMockedHttpClient(), 'Geocoder PHP/bpost Provider/bpost Test');
+        $provider->geocodeQuery(GeocodeQuery::create('::ffff:88.188.221.14'));
+    }
+
+    public function testGeocodeQuery()
+    {
+        $provider = new bpost($this->getHttpClient(), 'Geocoder PHP/bpost Provider/bpost Test');
+        $results = $provider->geocodeQuery(GeocodeQuery::create('35 avenue jean de bologne 1020 bruxelles'));
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        $this->assertCount(1, $results);
+
+        /** @var \Geocoder\Model\Address $result */
+        $result = $results->first();
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals(50.896344, $result->getCoordinates()->getLatitude(), '', 0.01);
+        $this->assertEquals(4.3605984, $result->getCoordinates()->getLongitude(), '', 0.01);
+        $this->assertEquals('35', $result->getStreetNumber());
+        $this->assertEquals('AVENUE JEAN DE BOLOGNE', $result->getStreetName());
+        $this->assertEquals('1020', $result->getPostalCode());
+        $this->assertEquals('BRUXELLES', $result->getLocality());
+        $this->assertEquals('BELGIQUE', $result->getCountry());
+    }
+}
